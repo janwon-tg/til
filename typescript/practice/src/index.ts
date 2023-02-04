ea84f2746affe919ce2238a51c469030b63bdec8
janwon.kim <janwon.kim@topgate.co.jp>
2023-02-04 23:19:41 +0900

    引数周りの確認

--- typescript/practice/src/index.ts
+++ typescript/practice/src/index.ts
@@ -1,6 +1,16 @@
 const message: string = "Hello, world!";
 const message2: string = "Hello, world2!";
 
+type Human = {
+	height: number;
+	weight: number;
+}
+
+interface Human2 {
+	height: number;
+	weight: number;
+}
+
 console.log(message);
 console.log(message2);
 console.log(range(5, 10.0));
@@ -20,4 +30,30 @@ function range(min: number, max: number): number[] {
   return result;
 }
 
+/*
+   src/index.ts:33:28 - error TS2339: Property 'h' does not exist on type 'Human'.
+   33 const calcBMI3 = function({ h, w }: Human): number {
+   src/index.ts:33:31 - error TS2339: Property 'w' does not exist on type 'Human'.
+   33 const calcBMI = function({ h, w }: Human): number {
+*/
+//const calcBMI3 = function({ h, w }: Human): number {
+//	return weight / height ** 2;
+//};
+const calcBMI = function({ height, weight }: Human): number {
+	return weight / height ** 2;
+};
 
+const calcBMI2 = function(h: Human): number {
+	return h.weight / h.height ** 2;
+};
+
+const taro: Human = { height:1.8, weight:80 };
+const jiro: Human2 = { height:1.3, weight:36 };
+
+calcBMI(taro);
+calcBMI(jiro);
+calcBMI({height:1.7, weight:77});
+
+calcBMI2(taro);
+calcBMI2(jiro);
+calcBMI2({height:1.7, weight:77});
