e39d9b449ae9d5b42e2c23e53c6f660bb80ba162
janwon.kim <janwon.kim@topgate.co.jp>
2023-02-04 23:07:51 +0900

    add function 1

--- typescript/practice/src/index.ts
+++ typescript/practice/src/index.ts
@@ -1,4 +1,23 @@
 const message: string = "Hello, world!";
 const message2: string = "Hello, world2!";
+
 console.log(message);
 console.log(message2);
+console.log(range(5, 10.0));
+helloNtimes(5);
+
+function helloNtimes(n: number): void {
+  for (let i = 0; i < n; i++) {
+    console.log("Hello World!!");
+  }
+}
+
+function range(min: number, max: number): number[] {
+  const result = [];
+  for (let i = min; i <= max; i++) {
+    result.push(i);
+  }
+  return result;
+}
+
+
